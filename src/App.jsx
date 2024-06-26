d68b3aed03184705111a2ac3fd0492505627faa5
Ruby Izekor <1izekorruby@gmail.com>
2024-06-26 21:39:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Nav, Portfolio, Blog, Network, About, Carousel, Footer } from './compon
 function App() {
   return (
     <div className='bg-[#D9D9D9]'>
+        <div className="absolute top-0 left-0 w-full h-screen bg-[teal] flex items-center justify-center md:hidden">
+            <p className="text-lg text-white">This is only available in desktop applications</p>
+            <p className="text-white text-sm">Open the website on a desktop to enjoy</p>
+        </div>
       <Nav/>
       <Carousel/>
       <About/>
