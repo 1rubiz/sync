9170f2afd6c726d3dcf7b93e496a62b2e10e3af8
Ruby Izekor <1izekorruby@gmail.com>
2024-06-26 21:53:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
-import { Nav, Portfolio, Blog, Network, About, Carousel, Footer } from './components/pages'
-
+import { Nav, Loading, Portfolio, Blog, Network, About, Carousel, Footer } from './components/pages'
+import {useState, useEffect} from "react"
 function App() {
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+  })
   return (
     <div className='bg-[#D9D9D9]'>
-        <div className="absolute top-0 left-0 w-full h-screen bg-[teal] flex items-center justify-center md:hidden">
+      {
+        loading && (<Loading/>)
+        }
+        <div className="fixed z-10 top-0 left-0 w-full h-screen bg-[teal] flex items-center justify-center md:hidden">
             <p className="text-lg text-white">This is only available in desktop applications</p>
             <p className="text-white text-sm">Open the website on a desktop to enjoy</p>
         </div>
